d5130e62710ebd3e3e9ef7fa52bd45f8650e152c
PARK <PARK@PARK-PC>
2019-05-22 23:52:12 +0900

    나의테스트

--- Cloud/src/main/java/com/example/demo/CloudApplication.java
+++ Cloud/src/main/java/com/example/demo/CloudApplication.java
@@ -8,6 +8,9 @@ public class CloudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudApplication.class, args);
+		
+		
+	    System.out.println("my test");
 	}
 
 }
